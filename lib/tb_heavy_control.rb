ede42621b9aef9d3a956e15fe2f4fb4120abd650
Roman Kolesnev <rvkolesnev@gmail.com>
2016-03-08 00:50:36 +0300

    Add trailblazer-rails stuff

--- lib/tb_heavy_control.rb
+++ lib/tb_heavy_control.rb
@@ -5,3 +5,15 @@ module TbHeavyControl
 end
 
 require 'tb_heavy_control/railtie'
+
+# stuff below is copy from trailblazer-rails gem
+# stuff inside lib/trailblazer folder too =)
+
+require 'trailblazer/operation'
+# TODO: remove that once i18n, validations etc in Reform/AM are sorted.
+Trailblazer::Operation.contract_class.class_eval do
+  def self.name
+    # for whatever reason, validations climb up the inheritance tree and require _every_ class to have a name (4.1).
+    'Reform::Form'
+  end
+end
--- lib/tb_heavy_control/railtie.rb
+++ lib/tb_heavy_control/railtie.rb
@@ -12,6 +12,21 @@ module TbHeavyControl
       app.config.eager_load_paths -= [path]
     end
 
+    # this initializer is a copy from trailblazer-rails gem
+    initializer 'tb_heavy_control.trailblazer-rails_stuff' do
+      require 'trailblazer/autoloading'
+
+      require 'trailblazer/operation/model'
+      require 'trailblazer/operation/model/active_model'
+      Trailblazer::Operation::Model::DSL.module_eval do
+        include Trailblazer::Operation::Model::ActiveModel # ::contract.
+      end
+
+      Trailblazer::Operation.class_eval do
+        autoload :Responder, 'trailblazer/operation/responder'
+      end
+    end
+
     initializer 'tb_heavy_control.concepts_loader', after: :load_config_initializers do |app|
       ActionDispatch::Reloader.to_prepare do
         TbHeavyControl::Railtie.concepts_loader(app)
