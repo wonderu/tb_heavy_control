1632acec7ba4bd5c0ec8ff62b7d4aa18c9d3afe6
Roman Kolesnev <rvkolesnev@gmail.com>
2016-03-07 21:19:27 +0300

    Add #load_folder feature to configuration

--- lib/tb_heavy_control/configurable.rb
+++ lib/tb_heavy_control/configurable.rb
@@ -5,29 +5,38 @@ module TbHeavyControl
     def config(&block)
       raise 'tb_heavy_control config cannot run without block' if block.nil?
       @load_order ||= []
-      @context = Pathname.new('')
+      @context = Rails.root.join('app', 'concepts')
       instance_eval(&block)
     end
 
     private
 
     def load_file(original_path)
-      array_form    = Array(original_path)
+      array_form    = Array(original_path).map(&:to_s)
       last_element  = array_form.last
 
       array_form[-1] = last_element + '.rb' unless last_element[-3..-1] == '.rb'
 
-      relative_path = @context.join(*array_form)
-      path = Rails.root.join 'app', 'concepts', relative_path
+      path = @context.join(*array_form)
       raise "Cannot find file: #{path}" unless path.file?
-      @load_order << path
+      @load_order << path unless @load_order.include?(path)
     end
 
     def context(path)
       previous_context = @context
       @context = previous_context.join(*Array(path))
+      raise "#{@context} isn't a directory" unless @context.directory?
       yield
       @context = previous_context
+    end
+
+    def load_folder(original_path = '')
+      array_form  = Array(original_path)
+      folder      = @context.join(*array_form)
+      raise "#{folder} isn't a directory" unless folder.directory?
+
+      rb_files = folder.entries.select { |pn| pn.to_s[-3..-1] == '.rb' }
+      rb_files.each { |file| load_file folder.join(file) }
     end
   end
 end
